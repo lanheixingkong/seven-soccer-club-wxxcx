0e6f3b7056d7babdad6b0bdb8a45c14ab85bcdd0
谌磊 <Lei@chenleideMacBook-Air.local>
2018-06-06 23:16:20 +0800

    完成赛程

--- pages/schedule/schedule.js
+++ pages/schedule/schedule.js
@@ -5,117 +5,93 @@ Page({
    * 页面的初始数据
    */
   data: {
-      schedules: [{
-        "round": "小组赛",
-        "matches": [{
-          "matchTime": "18-06-14 23:00",
-          "hostName": "俄罗斯",
-          "hostLogo": "http://duihui.qiumibao.com/zuqiu/eluosi.png",
-          "hostScore": 0,
-          "guestName": "沙特阿拉伯",
-          "guestLogo": "http://duihui.qiumibao.com/zuqiu/shatealabo.png",
-          "guestScore": 0,
-          "matchStatus": 0
-        },
-          {
-            "matchTime": "18-06-14 23:00",
-            "hostName": "俄罗斯",
-            "hostLogo": "http://duihui.qiumibao.com/zuqiu/eluosi.png",
-            "hostScore": 0,
-            "guestName": "沙特阿拉伯",
-            "guestLogo": "http://duihui.qiumibao.com/zuqiu/shatealabo.png",
-            "guestScore": 0,
-            "matchStatus": 1
-          },
-          {
-            "matchTime": "18-06-14 23:00",
-            "hostName": "俄罗斯",
-            "hostLogo": "http://duihui.qiumibao.com/zuqiu/eluosi.png",
-            "hostScore": 0,
-            "guestName": "沙特阿拉伯",
-            "guestLogo": "http://duihui.qiumibao.com/zuqiu/shatealabo.png",
-            "guestScore": 0,
-            "matchStatus": 1
-          }]
-      },
-        {
-          "round": "淘汰赛",
-          "matches": [{
-            "matchTime": "18-06-14 23:00",
-            "hostName": "俄罗斯",
-            "hostLogo": "http://duihui.qiumibao.com/zuqiu/eluosi.png",
-            "hostScore": 0,
-            "guestName": "沙特阿拉伯",
-            "guestLogo": "http://duihui.qiumibao.com/zuqiu/shatealabo.png",
-            "guestScore": 0,
-            "matchStatus": 1
-          },
-          {
-            "matchTime": "18-06-14 24:00",
-            "hostName": "俄罗斯",
-            "hostLogo": "http://duihui.qiumibao.com/zuqiu/eluosi.png",
-            "hostScore": 0,
-            "guestName": "沙特阿拉伯",
-            "guestLogo": "http://duihui.qiumibao.com/zuqiu/shatealabo.png",
-            "guestScore": 0,
-            "matchStatus": 0
-          }]
-        }]
+    schedules: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    this.loadSchedules()
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
+  },
+
+  loadSchedules() {
+    const that = this
+    wx.showLoading({
+      title: '加载中',
+    })
+    wx.request({
+      url: 'https://www.trueman.xyz/schedule/worldcup/2018',
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: function (res) {
+        wx.hideLoading()
+        //console.log(res.data)
+        const ret = res.data;
+        if (ret.state == 1) {
+          that.setData({
+            schedules: ret.data
+          })
+        }
+      },
+      fail: function(){
+        wx.showToast({
+          title: '加载失败',
+          icon: 'none',
+          duration: 2000
+        })
+      }
+    })
   }
 })
+
