dc8d857a480d0b99bac719dac2c8454e75a24fed
谌磊 <Lei@chenleideMacBook-Air.local>
2018-06-30 12:51:14 +0800

    增加用户列表，登录拦截

--- pages/user/user.js
+++ pages/user/user.js
@@ -1,4 +1,5 @@
 // pages/user/user.js
+const util = require('../../utils/util.js')
 //获取应用实例
 const app = getApp()
 
@@ -8,103 +9,79 @@ Page({
    * 页面的初始数据
    */
   data: {
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    rankings: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    if (app.globalData.userInfo) {
-      console.log("app" + app.globalData.userInfo)
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse) {
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        console.log("res" + res.userInfo)
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          console.log("res2" + res.userInfo)
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-    this.getUserInfo()
-  },
-  getUserInfo: function (e) {
-    if (e) {
-      app.globalData.userInfo = e.detail.userInfo
-      this.setData({
-        userInfo: e.detail.userInfo,
-        hasUserInfo: true
-      })
-    }
-
+  onLoad: function(options) {
+    this.loadUserRankings()
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-
+  onPullDownRefresh: function() {
+    this.loadUserRankings()
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
+  },
+
+  loadUserRankings() {
+    const that = this
+    console.log("util:" + util)
+    util.authRequest({
+      url: 'userInfo/ranking',
+      success: function(ret) {
+        if (ret.state == 1) {
+          that.setData({
+            rankings: ret.data
+          })
+        } else {
+          util.toast("加载失败")
+        }
+      }
+    })
   }
 })
--- utils/util.js
+++ utils/util.js
@@ -38,10 +38,63 @@ const toast = msg => {
     duration: 2000
   })
 }
-const baseUrl = "https://www.trueman.xyz/"
-//const baseUrl = "https://00bbc8ac.ngrok.io/"
+//const baseUrl = "https://www.trueman.xyz/"
+const baseUrl = "https://00bbc8ac.ngrok.io/"
 const getUrl = url => {
   return baseUrl + url
+}
+
+const authRequest = param => {
+  console.log("开始authRequest")
+  console.log(param)
+  let needLoading = true
+  if (param.loading && param.loading == false) {
+    needLoading = false
+  }
+
+  if (needLoading) {
+    wx.showLoading({
+      title: param.loadingMsg || '加载中',
+    })
+  }
+
+  wx.request({
+    url: getUrl(param.url),
+    header: {
+      'content-type': 'application/json', // 默认值
+      'token': getToken()
+    },
+    success: function(res) {
+      if (needLoading) {
+        wx.hideLoading()
+      }
+
+      //console.log(res.data)
+      const ret = res.data
+      console.log(ret)
+      if (ret.state == 401) {
+        toast("请重新登录")
+        wx.switchTab({
+          url: '../index/index'
+        })
+      } else {
+        if (param.success) {
+          param.success(ret)
+        }
+      }
+    },
+    fail: function() {
+      if (param.fail) {
+        param.fail()
+      } else {
+        wx.showToast({
+          title: '加载失败',
+          icon: 'none',
+          duration: 2000
+        })
+      }
+    }
+  })
 }
 
 module.exports = {
@@ -49,5 +102,6 @@ module.exports = {
   getToken: getToken,
   saveToken: saveToken,
   toast: toast,
-  getUrl: getUrl
+  getUrl: getUrl,
+  authRequest: authRequest
 }
