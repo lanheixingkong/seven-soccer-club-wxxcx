c0de0060026b7f8f69d54871169a51cf4071a189
谌磊 <Lei@chenleideMacBook-Air.local>
2018-06-30 20:19:13 +0800

    修复index头像不显示问题

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,37 +7,31 @@ Page({
   data: {
     motto: 'Hello World',
     userInfo: {},
-    hasUserInfo: true,
+    hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
   onLoad: function() {
     this.checkToken()
   },
+  onShow: function() {
+    console.log("hasUserInfo:" + this.data.hasUserInfo)
+    console.log("canIUse:" + this.data.canIUse)
+    console.log(this.data.userInfo)
+  },
   getUserInfo: function() {
-    const that = this
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true,
-        motto: "Hello " + app.globalData.userInfo.nickName
-      })
-      that.saveUserInfo()
-    } else if (this.data.canIUse) {
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
+    if (!this.data.userInfo.nickName) {
+      const that = this
+      if (app.globalData.userInfo) {
         this.setData({
-          userInfo: res.userInfo,
+          userInfo: app.globalData.userInfo,
           hasUserInfo: true,
           motto: "Hello " + app.globalData.userInfo.nickName
         })
         that.saveUserInfo()
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
+      } else if (this.data.canIUse) {
+        // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+        // 所以此处加入 callback 以防止这种情况
+        app.userInfoReadyCallback = res => {
           this.setData({
             userInfo: res.userInfo,
             hasUserInfo: true,
@@ -45,7 +39,20 @@ Page({
           })
           that.saveUserInfo()
         }
-      })
+      } else {
+        // 在没有 open-type=getUserInfo 版本的兼容处理
+        wx.getUserInfo({
+          success: res => {
+            app.globalData.userInfo = res.userInfo
+            this.setData({
+              userInfo: res.userInfo,
+              hasUserInfo: true,
+              motto: "Hello " + app.globalData.userInfo.nickName
+            })
+            that.saveUserInfo()
+          }
+        })
+      }
     }
   },
   login: function() {
@@ -65,6 +72,7 @@ Page({
               if (ret.state == 1) {
                 console.log("登录成功")
                 util.saveToken(ret.data)
+                that.getUserInfo()
                 //登录成功
                 wx.navigateTo({
                   url: '../user/user'
@@ -77,7 +85,7 @@ Page({
                   hasUserInfo: false
                 })
               } else {
-                  util.toast("登录失败")
+                util.toast("登录失败")
               }
             },
             fail: function() {
@@ -104,6 +112,7 @@ Page({
           const ret = res.data
           if (ret.state == 1) {
             console.log("token有效")
+            that.getUserInfo()
             wx.navigateTo({
               url: '../user/user'
             })
@@ -130,6 +139,9 @@ Page({
     const that = this
     const userInfo = this.data.userInfo
     const token = util.getToken()
+    wx.showLoading({
+      title: '登录中'
+    })
     wx.request({
       url: util.getUrl('index/userinfo'),
       header: {
@@ -137,9 +149,13 @@ Page({
       },
       data: userInfo,
       success: function(res) {
+        wx.hideLoading()
         const ret = res.data
         if (ret.state == 1) {
           util.toast("登录成功")
+          that.setData({
+            hasUserInfo: true
+          })
           wx.navigateTo({
             url: '../user/user'
           })
@@ -148,13 +164,19 @@ Page({
         }
       },
       fail: function() {
+        wx.hideLoading()
         console.log("valid请求失败")
       }
     })
   },
-  bindViewTap: function(e){
-    wx.navigateTo({
-      url: '../user/user',
-    })
+  bindViewTap: function(e) {
+    if (this.data.hasUserInfo && this.data.userInfo.nickName) {
+      wx.navigateTo({
+        url: '../user/user',
+      })
+    } else {
+      this.checkToken()
+    }
+
   }
 })
